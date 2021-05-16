1db5523147a016ae09f7430acfe68b79a139451c
Robert Szarvas <robisz98@gmail.com>
2021-05-16 21:18:44 +0300

    game functionality and responsiveness

--- public/games/numbers-game/game.js
+++ public/games/numbers-game/game.js
@@ -1,18 +1,20 @@
 const baseImagePath = "../../images/numbers-game";
 
 var availableObjects = ['apples', 'ball', 'people'];
-var currentObject = 0;
+var correctAnswer = [3, 1, 2];
+var currentObject = -1;
 
 function initGame() {
     setBackgroundImageForObjects();
 }
 
-function onFigureClicked() {
-    setBackgroundImageForObjects();
+function onFigureClicked(number) {
+	if (number == correctAnswer[currentObject])
+		setBackgroundImageForObjects();
 }
 
 function setBackgroundImageForObjects() {
-    document.getElementsByClassName("objects")[0].style.backgroundImage = `url(${baseImagePath}/${availableObjects[currentObject++]}.jpg)`;
+    document.getElementsByClassName("objects")[0].style.backgroundImage = `url(${baseImagePath}/${availableObjects[++currentObject]}.jpg)`;
 }
 
 function onQuestionMarkClicked() {
